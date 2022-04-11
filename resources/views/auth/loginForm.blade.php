8a8ea4783d03d9e65cb2d25d5fbea4550efc7eac
ariqluth <ttzluthfi@gmail.com>
2022-04-11 15:42:27 +0700

    Login default laravel

---
+++ resources/views/auth/loginForm.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+   <section>
+       <div class="imgBx">
+       </div>
+       <div class="contentBx">
+
+       </div>
+   </section>
+</body>
+</html>
